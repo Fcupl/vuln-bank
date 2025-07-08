a42231b224da5428c860d0b8d95faf31d6446818
Fcupl <127294009+Fcupl@users.noreply.github.com>
2025-07-08 11:34:14 +0700

    Update send_telegram.py

--- .github/scripts/send_telegram.py
+++ .github/scripts/send_telegram.py
@@ -6,6 +6,21 @@ bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 chat_id = os.getenv("TELEGRAM_CHAT_ID")
 message = sys.argv[1] if len(sys.argv) > 1 else "Tidak ada pesan."
 
+repo = os.getenv("GITHUB_REPOSITORY") ##
+run_id = os.getenv("GITHUB_RUN_ID") ##
+
+message = f"""
+🚨 *Pipeline DevSecOps Selesai!*
+
+📦 Secret Scanning (Gitleaks)
+🔍 SAST (Semgrep)
+📦 SCA (Snyk)
+🧪 DAST (ZAP)
+
+📊 Status: *{status_summary}*
+🔗 [Lihat detail pipeline](https://github.com/{repo}/actions/runs/{run_id})
+"""
+
 url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 data = {
     "chat_id": chat_id,
@@ -16,3 +31,4 @@ data = {
 
 response = requests.post(url, data=data)
 print(f"Telegram response: {response.status_code}")
+
