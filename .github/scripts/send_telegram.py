e66ba707f21aa82d30554c09b32ac82f32859974
Fcupl <127294009+Fcupl@users.noreply.github.com>
2025-07-08 11:23:34 +0700

    Update send_telegram.py

--- .github/scripts/send_telegram.py
+++ .github/scripts/send_telegram.py
@@ -7,12 +7,11 @@ chat_id = os.getenv("TELEGRAM_CHAT_ID")
 message = sys.argv[1] if len(sys.argv) > 1 else "Tidak ada pesan."
 
 url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-
 data = {
     "chat_id": chat_id,
     "text": message,
     "parse_mode": "Markdown",
-    "disable_web_page_preview": True,
+    "disable_web_page_preview": True
 }
 
 response = requests.post(url, data=data)
